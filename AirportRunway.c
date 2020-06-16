b7ced2e866ed62e0ccb6237187258820c600b61b
Michel Natan Feyh <63029905+michelnatan@users.noreply.github.com>
2020-06-15 22:37:30 -0300

    Update AirportRunway.c

--- AirportRunway.c
+++ AirportRunway.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
 #define TRUE 1
 #define FALSE 0
+#define TAM 30
+#define QTD 5
 
-#define TAM 31
-#define TAMFILA 100
-
-// CRIANDO A ESTRUTURA DO AVIÃO
 struct aviao {
     char prefixo[TAM];
     char nome[TAM];
@@ -16,102 +13,179 @@ struct aviao {
 };
 typedef struct aviao TipoAviao;
 
-// CRIANDO A ESTRUTURA DA FILA DE AVIÕES
-struct filaaviao {
-    TipoAviao avioes[TAMFILA];
-    int inicio;
-    int fim;
+struct fila {
+    TipoAviao p[TAM];
+    int primeiro;
+    int ultimo;
     int tam;
 };
-typedef struct filaaviao TipoFilaAviao;
+typedef struct fila TipoFila;
 
-/* INICIALIZA FILA */
-TipoFilaAviao inicializar(TipoFilaAviao fila) {
-    fila.inicio = 0;
-    fila.fim = 0;
+TipoFila inicializar(TipoFila fila);
+TipoFila le_aviao (TipoAviao aviao,TipoFila fila);
+TipoAviao insere_aviao (TipoAviao aviao);
+TipoFila qtd_aviao(TipoFila fila);
+TipoFila imprimir (TipoFila fila);
+TipoFila decolar (TipoFila fila);
+
+int main(){
+    TipoFila fila;
+    TipoAviao aviao;
+    int menu;
+    char continuar = 'S';
+
+    fila = inicializar(fila);
+
+    while(continuar == 's' || continuar == 'S'){
+    printf("\t## MENU ##\n\n");
+    printf("(1) Adicionar aviao a pista de decolagem\n");
+    printf("(2) Verificar quantidade de avioes aguardando na pista\n");
+    printf("(3) Autorizar a decolagem do primeiro aviao da fila\n");
+    printf("(4) Listar os dados de todos os avioes que estao na pista de decolagem\n");
+
+    printf("\nQual opcao voce deseja ?");
+    scanf("%d",&menu);
+    fflush(stdin);
+
+    system("cls");
+
+    switch (menu){
+        case 1:
+            fila = le_aviao(aviao,fila);
+            break;
+        case 2:
+            qtd_aviao(fila);
+            break;
+        case 3:
+            fila = decolar(fila);
+            break;
+        case 4:
+            imprimir(fila);
+            break;
+
+    }
+
+    printf("Deseja voltar ao menu ?\t (S/s - Sim | N/n - Nao) ");
+    scanf("%s",&continuar);
+    fflush(stdin);
+
+    system("cls");
+
+    }
+
+}
+
+TipoFila inicializar(TipoFila fila){ //Inicializando a fila
+    fila.primeiro = 0;
+    fila.ultimo = 0;
     fila.tam = -1;
     return fila;
 }
-
-/* VERIFICA SE A FILA ESTÁ VAZIA */
-int estaVazia(TipoFilaAviao fila) {
-    if (fila.tam == -1)
+/*
+int estaVazia(TipoFila fila){
+    if (fila.primeiro == -1)
         return TRUE;
     else
         return FALSE;
+}'
+*/
+
+TipoFila le_aviao (TipoAviao aviao,TipoFila fila){ // Inserindo aviao na pista
+    int aux=0;
+    if(fila.ultimo < QTD ){
+        aux = fila.ultimo;
+    }
+        if (fila.ultimo == QTD){
+            if(fila.ultimo == fila.primeiro){
+            aux = fila.ultimo;
+        }else{
+            aux = 0;
+            }
+        }
+            if(aux != fila.primeiro || fila.tam == -1){
+                fila.ultimo = aux;
+                aviao = insere_aviao(aviao); // chama fun�ao para usuario preencher os dados do aviao
+                fila.p[fila.ultimo++] = aviao;
+                fila.tam++;
+                aux++;
+            }else{
+                printf("Overflow !!!\n\n");
+            }
+       return fila;
 }
 
-/* INSERINDO UM AVIÃO NA FILA */
-TipoFilaAviao entraFila(TipoAviao aviao, TipoFilaAviao fila) {
-    if (fila.tam < TAMFILA)
-        fila.avioes[fila.fim++] = aviao;
-    else
-        printf("Overflow !!!\n");
-    fila.tam++;
+TipoAviao insere_aviao (TipoAviao aviao){
+    printf("PREFIXO: ");
+    scanf("%[^\n]",aviao.prefixo);
+    fflush(stdin);
+    printf("NOME: ");
+    scanf("%[^\n]",aviao.nome);
+    fflush(stdin);
+    printf("CIA: ");
+    scanf("%[^\n\n]",aviao.cia);
+    fflush(stdin);
+    return aviao;
+}
+
+TipoFila qtd_aviao(TipoFila fila){  // Verifica quantidade de avioes da fila
+    int i;
+    if(fila.tam >= 0){
+        i = fila.tam;
+        printf("\n%d avioes estao aguardando na pista de decolagem no momento\n\n", ++i);
+    }else{
+        printf("\nA pista de decolagem esta vazia\n\n");
+    }
     return fila;
 }
 
-/* REMOVENDO UM AVIÃO DA FILA */
-TipoFilaAviao saiFila(TipoFilaAviao fila) {
-    if (estaVazia(fila))
-        printf("Fila Vazia !!!");
-    else
-        printf("Avião que saiu da fila: %s\n", fila.avioes[fila.inicio++].prefixo);
-    fila.tam--;
+TipoFila imprimir (TipoFila fila){  // Imprime lista
+    int i = -1;
+    int aux = fila.primeiro;
+    int aux2 = fila.primeiro;
+
+    if(fila.tam == -1 ){
+    printf("\nA pista de decolagem esta vazia\n\n");
+    }else{
+        while (i < fila.tam){
+            if(fila.primeiro == QTD){
+                aux = 0;
+            }
+                if(aux != fila.primeiro || fila.tam != -1 ){
+                    fila.primeiro = aux;
+                    i++;
+                    printf("#%d\t Prefixo: %s\tNome: %s\tCia: %s\n\n",i+1,fila.p[fila.primeiro].prefixo,fila.p[fila.primeiro].nome,fila.p[fila.primeiro].cia);
+                    fila.primeiro++;
+                    aux++;
+                }
+            }
+        }
+}
+
+TipoFila decolar (TipoFila fila){   //Decola aviao
+    int aux=0;
+    int i = fila.primeiro;
+    if (fila.tam == -1){
+        printf("\nFila Vazia !!!\n\n");
+    }
+        else{
+            if(fila.primeiro < QTD){
+                aux = fila.primeiro;
+            }
+                if(fila.primeiro == QTD){
+                    aux = 0;
+                }
+                    if(aux != fila.ultimo || fila.tam != -1 ){
+                        fila.primeiro = aux;
+                        printf("Aviao decolando...\n#%d\t Prefixo: %s\tNome: %s\tCia: %s\n\n",i+1,fila.p[fila.primeiro].prefixo,fila.p[fila.primeiro].nome,fila.p[fila.primeiro].cia);
+                        fila.primeiro++;
+                        fila.tam--;
+                        aux++;
+                            if(fila.tam == -1){
+                                fila.primeiro = 0;
+                                fila.ultimo = 0;
+                                fila.tam = -1;
+                            }
+                    }
+            }
     return fila;
-}
-
-void imprimir(TipoFilaAviao fila) {
-    int i = fila.inicio;
-    printf("FILA ATUAL\n");
-    while (i < fila.fim)
-        printf("%s\n", fila.avioes[i++].prefixo);
-}
-
-int main () {
-    TipoFilaAviao fila;
-    TipoAviao aviao;
-    fila = inicializar(fila);
-
-    strcpy(aviao.prefixo, "A");strcpy(aviao.nome, "AA");strcpy(aviao.cia, "AAA");
-    fila = entraFila(aviao, fila);
-    imprimir(fila);
-    getchar();
-
-    strcpy(aviao.prefixo, "B");strcpy(aviao.nome, "BB");strcpy(aviao.cia, "BBB");
-    fila = entraFila(aviao, fila);
-    imprimir(fila);
-    getchar();
-
-    strcpy(aviao.prefixo, "C");strcpy(aviao.nome, "CC");strcpy(aviao.cia, "CCC");
-    fila = entraFila(aviao, fila);
-    imprimir(fila);
-    getchar();
-
-    fila = saiFila(fila);
-    imprimir(fila);
-    getchar();
-
-    strcpy(aviao.prefixo, "D");strcpy(aviao.nome, "DD");strcpy(aviao.cia, "DDD");
-    fila = entraFila(aviao, fila);
-    imprimir(fila);
-
-    fila = saiFila(fila);
-    imprimir(fila);
-    getchar();
-
-    fila = saiFila(fila);
-    imprimir(fila);
-    getchar();
-
-    fila = saiFila(fila);
-    imprimir(fila);
-    getchar();
-
-    fila = saiFila(fila);
-    getchar();
-    imprimir(fila);
-
-    getchar();
-    return 0;
 }
